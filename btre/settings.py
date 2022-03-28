3da3feb68d1873cd3ca74df6f1630c8f0d804c4f
Ben <benmungovan@hotmail.com>
2022-03-28 16:31:49 +0100

    created realtors and listings in admin

--- btre/settings.py
+++ btre/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'salamanc',
+        'HOST': 'localhost'
+
+
     }
 }
 
@@ -124,6 +129,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+# Media Folder Settings 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
